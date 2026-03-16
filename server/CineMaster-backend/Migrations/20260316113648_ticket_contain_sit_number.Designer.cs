9f03a0084bca612a81d3397a2ed226b54b4e3735
Denis Nikitin <den221001@gmail.com>
2026-03-16 12:17:20 +0000

    feat: append sit number for ticket

--- server/CineMaster-backend/Migrations/20260316113648_ticket_contain_sit_number.Designer.cs
+++ server/CineMaster-backend/Migrations/20260316113648_ticket_contain_sit_number.Designer.cs
@@ -4,6 +4,7 @@ using CineMaster_backend.src;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CineMaster_backend.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20260316113648_ticket_contain_sit_number")]
+    partial class ticket_contain_sit_number
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/CineMaster-backend/Migrations/20260316113648_ticket_contain_sit_number.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineMaster_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ticket_contain_sit_number : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SitNumber",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SitNumber",
+                table: "Ticket");
+        }
+    }
+}
